45c8f0aa4c61bc3d4e0387c23ef178a2e19031de
limengen16 <limengen16@users.noreply.github.com>
2019-07-23 10:12:36 +0800

    匹配域名

--- vue.config.js
+++ vue.config.js
@@ -3,6 +3,10 @@ module.exports = {
   publicPath: process.env.NODE_ENV === "production" ? "./" : "/",
   devServer: {
     disableHostCheck: true,
+    allowedHosts: [
+      //匹配单个域名
+      '188.131.237.111'
+    ],
     proxy:{
       '/run': {
         target: 'http://188.131.237.111:7001',
